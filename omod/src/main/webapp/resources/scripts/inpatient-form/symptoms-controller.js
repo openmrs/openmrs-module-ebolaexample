6ada9ee211df2de8a77fd262d71553fc81af6acc
Márcio Viegas <marcioaugustoviegas@gmail.com>
2014-11-04 20:08:00 -0200

    symptoms view model

--- omod/src/main/webapp/resources/scripts/inpatient-form/symptoms-controller.js
+++ omod/src/main/webapp/resources/scripts/inpatient-form/symptoms-controller.js
@@ -1,5 +1,21 @@
 var module = angular.module('inpatientForm')
 
-module.controller("symptomsController", function() {
-
+module.controller('symptomsController', function() {
+  $scope.viewModel = {
+    overallSymptoms: '',
+    dehydration: '',
+    bleeding: '',
+    jointMusclePain: '',
+    headche: '',
+    urinePain: '',
+    abdominalPain: '',
+    unableToEat: '',
+    unableToDrink: '',
+    difficultToSwallow: '',
+    difficultToBreathe: '',
+    fatique: '',
+    hiccups: '',
+    cought: '',
+    rash: ''
+  };
 });
