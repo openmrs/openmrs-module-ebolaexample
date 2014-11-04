ccfda019e577b388786712317264018825924f7c
Ramon Gonçalves & Marcos Matos <raigons@gmail.com>
2014-11-04 20:36:51 -0200

    noobice :P

--- omod/src/main/webapp/resources/scripts/inpatient-form/vital-signs-controller.js
+++ omod/src/main/webapp/resources/scripts/inpatient-form/vital-signs-controller.js
@@ -1,17 +1,17 @@
 var module = angular.module('inpatientForm')
 
 module.controller("vitalSignsController", function($scope) {
-	$scope.viewModel = {
-		currentConsciouness: "",
-		temperature: "",
-		oxygenSaturation: "",
-		respiratoryRate: "",
-		heartRate: "",
-		systolicBP: "",
-		diastolicBP: "",
-		raisedJVP: "",
-		capillaryRefillTime: "",
-		abdomenTender: "",
-		paleAnaemia: ""
-	};
+  $scope.viewModel = {
+    currentConsciouness: "",
+    temperature: "",
+    oxygenSaturation: "",
+    respiratoryRate: "",
+    heartRate: "",
+    systolicBP: "",
+    diastolicBP: "",
+    raisedJVP: "",
+    capillaryRefillTime: "",
+    abdomenTender: "",
+    paleAnaemia: ""
+  };
 });
