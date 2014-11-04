e17735c0f05176fb2690f6ebffc25d2a435d45d6
Mário Areias <mario.s.s.areias@gmail.com>
2014-11-04 21:23:03 -0200

    adding functions to create questions and answers

--- omod/src/main/webapp/resources/scripts/inpatient-form/client-server.js
+++ omod/src/main/webapp/resources/scripts/inpatient-form/client-server.js
@@ -2,4 +2,23 @@ var module = angular.module('inpatientForm')
 
 module.service("clientServerService", function() {
 
+
+  function createQuestion(formId, conceptType, conceptUuid) {
+    var question = {
+      id: formId,
+      type: conceptType,
+      conceptId: conceptUuid
+    }
+    return question;
+  }
+
+
+  function createAnswer(formId, conceptUuid) {
+    var question = {
+      id: formId,
+      conceptId: conceptUuid
+    }
+    return question;
+  }
+
 });
