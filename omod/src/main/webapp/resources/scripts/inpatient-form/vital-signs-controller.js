682909f4f28db90f8df8c7aa51fad47bfcb96383
Ramon Gonçalves <raigons@gmail.com>
2014-11-04 19:57:15 -0200

    add vital signs empty view-model

--- omod/src/main/webapp/resources/scripts/inpatient-form/vital-signs-controller.js
+++ omod/src/main/webapp/resources/scripts/inpatient-form/vital-signs-controller.js
@@ -1,5 +1,17 @@
 var module = angular.module('inpatientForm')
 
-module.controller("vitalSignsController", function() {
-
+module.controller("vitalSignsController", function($scope) {
+	$scope.viewModel = {
+		currentConsciouness: "",
+		temperature: "",
+		oxygenSaturation: "",
+		respiratoryRate: "",
+		heartRate: "",
+		systolicBP: "",
+		diastolicBP: "",
+		raisedJVP: "",
+		capillaryRefillTime: "",
+		abdomenTender: "",
+		paleAnaemia: ""
+	};
 });
