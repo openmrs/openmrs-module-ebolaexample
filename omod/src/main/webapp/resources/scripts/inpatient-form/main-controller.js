e1364c736f538f157e1f449f2d3228876d0a765d
Márcio Viegas <marcioaugustoviegas@gmail.com>
2014-11-06 21:38:24 -0200

    factory observations

--- omod/src/main/webapp/resources/scripts/inpatient-form/main-controller.js
+++ omod/src/main/webapp/resources/scripts/inpatient-form/main-controller.js
@@ -1,61 +1,70 @@
 var module = angular.module('inpatientForm');
 
-module.controller('MainController', function ($scope, observationsFactory) {
-    var createView = function (name, file, description) {
-            return {
-                name: name,
-                file: file,
-                description: description
-            }
-        }, activeView = undefined,
-        initialize = function () {
-            activeView = $scope.views[0];
-        },
-        getActiveViewIndex = function () {
-            return _.indexOf($scope.views, activeView);
-        };
-
-    $scope.views = [
-        createView('vital-signs', 'vital-signs.html', 'VITAL SIGNS'),
-        createView('hydration', 'hydration.html', 'HYDRATION'),
-        createView('symptoms', 'symptoms.html', 'SYMPTOMS'),
-        createView('daily-management', 'daily-management.html', 'DAILY MGMT')
-    ];
-
-    $scope.shouldDisplay = function (view) {
-        return activeView === view;
+module.controller('MainController', function($scope, observationsFactory) {
+  var createView = function(name, file, description) {
+      return {
+        name: name,
+        file: file,
+        description: description
+      }
+    },
+    activeView = undefined,
+    initialize = function() {
+      activeView = $scope.views[0];
+    },
+    getActiveViewIndex = function() {
+      return _.indexOf($scope.views, activeView);
     };
 
-    $scope.shouldDisplayBackButton = function () {
-        return getActiveViewIndex() !== 0;
-    };
+  $scope.views = [
+    createView('vital-signs', 'vital-signs.html', 'VITAL SIGNS'),
+    createView('hydration', 'hydration.html', 'HYDRATION'),
+    createView('symptoms', 'symptoms.html', 'SYMPTOMS'),
+    createView('daily-management', 'daily-management.html', 'DAILY MGMT')
+  ];
 
-    $scope.shouldDisplayNextButton = function () {
-        return getActiveViewIndex() !== $scope.views.length-1;
-    };
+  $scope.shouldDisplay = function(view) {
+    return activeView === view;
+  };
 
-    $scope.shouldDisplayFinishButton = function () {
-        return getActiveViewIndex() === $scope.views.length-1;
-    };
+  $scope.shouldDisplayBackButton = function() {
+    return getActiveViewIndex() !== 0;
+  };
 
-    $scope.next = function () {
-        activeView = $scope.views[getActiveViewIndex() + 1];
-    };
+  $scope.shouldDisplayNextButton = function() {
+    return getActiveViewIndex() !== $scope.views.length - 1;
+  };
 
-    $scope.back = function () {
-        activeView = $scope.views[getActiveViewIndex() - 1];
-    };
+  $scope.shouldDisplayFinishButton = function() {
+    return getActiveViewIndex() === $scope.views.length - 1;
+  };
 
-    $scope.finish= function () {
-        alert('Not there yet!');
-    };
+  $scope.next = function() {
+    activeView = $scope.views[getActiveViewIndex() + 1];
+  };
 
-    $scope.display = function (view) {
-        activeView = view;
-    };
+  $scope.back = function() {
+    activeView = $scope.views[getActiveViewIndex() - 1];
+  };
 
-    initialize();
+  $scope.finish = function() {
+    alert('Not there yet!');
+  };
 
-    console.table(observationsFactory);
+  $scope.display = function(view) {
+    activeView = view;
+  };
+
+  initialize();
+
+  console.table(observationsFactory.get({
+    oralFluids: '',
+    dehydration: '',
+    urineOutput: '',
+    vomiting: '',
+    stoolFreq: '',
+    mainStool: '',
+    respiratoryRate: ''
+  }));
 
 });
--- omod/src/main/webapp/resources/scripts/inpatient-form/observations-factory.js
+++ omod/src/main/webapp/resources/scripts/inpatient-form/observations-factory.js
@@ -3,70 +3,57 @@ var module = angular.module('inpatientForm')
 module.factory("observationsFactory", function(conceptMappingFactory) {
   var that = this;
 
-  this.viewModel = {
-    oralFluids: '',
-    dehydration: '',
-    urineOutput: '',
-    vomiting: '',
-    stoolFreq: '',
-    mainStool: '',
-    respiratoryRate: ''
-  };
+  this.createObservations = function(modelQuestions) {
 
+    var postList = [];
 
-  var postList = [];
-  var conceptList = conceptMappingFactory;
+    angular.forEach(modelQuestions, function(modelQuestionValue, modelQuestionKey) {
+      angular.forEach(conceptMappingFactory, function(concept) {
+        var post = {};
 
-  angular.forEach(that.viewModel, function(modelQuestionValue, modelQuestionKey) {
-
-    angular.forEach(conceptMappingFactory, function(concept) {
-      var post = {};
-
-      if(concept.type === "symptom"){
-         var answerValue;
-         angular.forEach(concept.answers, function(answer) {
-            if(answer.id === modelQuestionValue){
+        if (concept.type === "symptom") {
+          var answerValue;
+          angular.forEach(concept.answers, function(answer) {
+            if (answer.id === modelQuestionValue) {
               answerValue = answer.conceptId;
             }
           });
 
-        post.concept = "1727AAAAAAAAAAAAAAAAAAAAAAAAAAAA";
-        post.groupMembers = [ 
-          {
+          post.concept = "1727AAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+          post.groupMembers = [{
             concept: "",
             value: concept.conceptId
-          },
-          {
-        concept:  "",
-        value: answerValue            
+          }, {
+            concept: "",
+            value: answerValue
+          }];
+        }
+
+        if (concept.id === modelQuestionKey) {
+          post.concept = concept.id;
+
+          if (concept.type === "non-coded") {
+            post.value = modelQuestionValue;
+          } else if (concept.type == "coded") {
+            angular.forEach(concept.answers, function(answer) {
+              if (answer.id === modelQuestionValue) {
+                post.value = answer.conceptId;
+              }
+            });
           }
-        ];
-      }
 
-      if (concept.id === modelQuestionKey) {
-        post.concept = concept.id;
-
-        if (concept.type === "non-coded") {
-          post.value = modelQuestionValue;
+          postList.push(post);
         }
+      });
 
-        if (concept.type == "coded") {
-          angular.forEach(concept.answers, function(answer) {
-            if(answer.id === modelQuestionValue){
-              post.value = answer.conceptId;
-            }
-          });
-        }
-
-        postList.push(post);
-      }
     });
 
+    return postList;
+  }
 
-  });
-
-
- return postList;
-
-
+  return {
+    get: function(modelQuestions) {
+      return that.createObservations(modelQuestions);
+    }
+  }
 });
