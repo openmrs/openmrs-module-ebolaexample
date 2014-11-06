a41a17591553cd382933b764a364a3be89307f00
Luiza Nunes <luhhsnunes@gmail.com>
2014-11-06 19:46:03 -0200

    Mapping controller daily management

--- omod/src/main/webapp/resources/scripts/inpatient-form/daily-management-controller.js
+++ omod/src/main/webapp/resources/scripts/inpatient-form/daily-management-controller.js
@@ -1,5 +1,14 @@
 var module = angular.module('inpatientForm')
 
 module.controller("dailyManagementController", function() {
-
+  $scope.viewModel = {
+    fluidManagement: '',
+    volume: '',
+    antimalarials: '',
+    antibiotics: '',
+    analgesics: ''
+  };
 });
+
+
+
