823aa08eacc3dc04456b7c8398e32cebb65a9bcb
Romulo Santos <romulobr@gmail.com>
2014-11-06 20:51:25 -0200

    refactors view handling on main controller.

--- omod/src/main/webapp/resources/scripts/inpatient-form/main-controller.js
+++ omod/src/main/webapp/resources/scripts/inpatient-form/main-controller.js
@@ -1,13 +1,31 @@
 var module = angular.module('inpatientForm');
 
-module.controller('MainController', function($scope) {
-    var activeView = 'vital-signs';
+module.controller('MainController', function ($scope) {
+    var createView = function (name, file, description) {
+            return {
+                name: name,
+                file: file,
+                description: description
+            }
+        }, activeView = undefined,
+        initialize = function () {
+            activeView = $scope.views[0];
+        };
 
-    $scope.shouldDisplay = function (target) {
-        return activeView === target;
+    $scope.views = [
+        createView('vital-signs', 'vital-signs.html', 'VITAL SIGNS'),
+        createView('hydration', 'hydration.html', 'HYDRATION'),
+        createView('symptoms', 'symptoms.html', 'SYMPTOMS'),
+        createView('daily-management', 'daily-management.html', 'DAILY MGMT')
+    ];
+
+    $scope.shouldDisplay = function (view) {
+        return activeView === view;
     };
 
-    $scope.display = function (target) {
-        activeView = target;
+    $scope.display = function (view) {
+        activeView = view;
     };
+
+    initialize();
 });
