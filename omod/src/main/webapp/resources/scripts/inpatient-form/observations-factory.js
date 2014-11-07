2a2775fb23d9b48fb0f497c186cf41dafa3acaea
Márcio Viegas <marcioaugustoviegas@gmail.com>
2014-11-06 22:11:04 -0200

    refactory trying to remove the dragon

--- omod/src/main/webapp/resources/scripts/inpatient-form/observations-factory.js
+++ omod/src/main/webapp/resources/scripts/inpatient-form/observations-factory.js
@@ -7,49 +7,66 @@ module.factory("observationsFactory", function(conceptMappingFactory) {
 
     var postList = [];
 
+    var symptomGrpup = {
+     concept: "1727AAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+     conceptQuestion: "1728AAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+     conceptAnswer: "1729AAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+    }
+
     angular.forEach(modelQuestions, function(modelQuestionValue, modelQuestionKey) {
       angular.forEach(conceptMappingFactory, function(concept) {
         var post = {};
 
-        if (concept.type === "symptom") {
-          var answerValue;
-          angular.forEach(concept.answers, function(answer) {
-            if (answer.id === modelQuestionValue) {
-              answerValue = answer.conceptId;
-            }
-          });
-
-          post.concept = "1727AAAAAAAAAAAAAAAAAAAAAAAAAAAA";
-          post.groupMembers = [{
-            concept: "1728AAAAAAAAAAAAAAAAAAAAAAAAAAAA",
-            value: concept.conceptId
-          }, {
-            concept: "1729AAAAAAAAAAAAAAAAAAAAAAAAAAAA",
-            value: answerValue
-          }];
-        }
 
         if (concept.id === modelQuestionKey) {
           post.concept = concept.id;
 
-          if (concept.type === "non-coded") {
-            post.value = modelQuestionValue;
-          } else if (concept.type == "coded") {
-            angular.forEach(concept.answers, function(answer) {
-              if (answer.id === modelQuestionValue) {
-                post.value = answer.conceptId;
-              }
-            });
-          }
+          if (that.isSymptom(concept.type)) {
+            post.concept = symptom.concept;
+            post.groupMembers = [{
+              concept: symptom.conceptQuestion,
+              value: concept.conceptId
+            }, {
+              concept: symptom.conceptAnswer,
+              value: that.findAnswer(answers, modelQuestionValue)
+            }];
 
-          postList.push(post);
+          } else if (that.isNonCode(concept.type)) {
+            post.value = modelQuestionValue;
+          } else if (that.isCode(concept.type)) {
+            post.value = that.findAnswer(answers, modelQuestionValue);
+          }
         }
+
+        postList.push(post);
       });
 
     });
 
     return postList;
   }
+
+  // Yeah, I know!
+  this.isSymptom = function(type) {
+    return type === "symptom";
+  };
+
+  this.isCode = function(type) {
+    return type === "non-coded";
+  };
+
+  this.isNonCode = function(type) {
+    return type === "non-coded";
+  };
+
+  this.findAnswer = function(answers, value) {
+    angular.forEach(answers, function(answer) {
+      if (answer.id === value) {
+        return answer.conceptId;
+      }
+    });
+
+  };
 
   return {
     get: function(modelQuestions) {
