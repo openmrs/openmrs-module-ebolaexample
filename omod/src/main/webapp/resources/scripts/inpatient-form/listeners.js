7505d67d7c0b3a4167f5fee24630f1fab0520e46
Mário Areias <mario.s.s.areias@gmail.com>
2014-11-10 18:51:52 -0200

    renaming variable to store checked value

--- omod/src/main/webapp/resources/scripts/inpatient-form/listeners.js
+++ omod/src/main/webapp/resources/scripts/inpatient-form/listeners.js
@@ -4,14 +4,14 @@ $(function(){
   $(".ebola-form .section").on("click", ".radio-button", function(event){
     var $radio = $("#" + $(this).attr("for"));
 
-    var previousValue = $radio.attr("test");
+    var previousValue = $radio.attr("check");
 
     if(previousValue === "true"){
       $radio.prop("checked", false);
-      $radio.attr("test", false);
+      $radio.attr("check", false);
       event.preventDefault();
     } else {
-      $radio.attr("test", true);
+      $radio.attr("check", true);
     }
 
     event.stopPropagation();
