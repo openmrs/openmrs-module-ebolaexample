6366b27af86d1e2f8da95700013d819c446a4780
Mário Areias <mario.s.s.areias@gmail.com>
2014-11-11 17:15:48 -0200

    organizing observation factory and also make coded post have multiple answers

--- omod/src/main/webapp/resources/scripts/inpatient-form/observations-factory.js
+++ omod/src/main/webapp/resources/scripts/inpatient-form/observations-factory.js
@@ -17,27 +17,19 @@ module.factory("observationsFactory", function(conceptMappingFactory) {
       angular.forEach(conceptMappingFactory, function(concept) {
         var post = {};
 
-
         if (concept.id === modelQuestionKey) {
-          post.concept = concept.conceptId;
-
           if (that.isSymptom(concept.type)) {
-            post.concept = symptom.concept;
-            post.groupMembers = [{
-              concept: symptom.conceptQuestion,
-              value: concept.conceptId
-            }, {
-              concept: symptom.conceptAnswer,
-              value: that.findAnswer(concept.answers, modelQuestionValue)
-            }];
+            post = that.createSymptomPost(symptom, concept, modelQuestionValue);
             postList.push(post);
           } else if (that.isNonCode(concept.type)) {
-            post.value = modelQuestionValue;
+            post = that.createNonCodedPost(concept, modelQuestionValue);
             postList.push(post);
           } else if (that.isCode(concept.type)) {
-            post.value = that.findAnswer(concept.answers, modelQuestionValue);
-            postList.push(post);
+            post = that.createCodedPosts(concept, modelQuestionValue);
           }
+
+          postList.push(post);
+          postList = _.flatten(postList);
         }
 
       });
@@ -45,6 +37,47 @@ module.factory("observationsFactory", function(conceptMappingFactory) {
     });
 
     return postList;
+  }
+
+
+  this.createCodedPosts = function(concept, value) {
+    var codedPostList = [];
+    var values = that.findAnswer(concept.answers, value, true);
+
+    _.each(values, function(value){
+      var codedPost = {};
+      codedPost.concept = concept.conceptId;
+      codedPost.value = value;
+      codedPostList.push(codedPost);
+    });
+
+    return codedPostList;
+
+  }
+
+  this.createNonCodedPost = function(concept, value) {
+      var post = {};
+      post.concept = concept.conceptId;
+      post.value = value;
+      return post;
+  }
+
+  this.createSymptomPost = function(symptom, concept, value) {
+    var post = {};
+    post.concept = symptom.concept;
+    post.groupMembers = that.createGroupMembers(symptom, concept, that.findAnswer(concept.answers, value));
+    return post;
+  }
+
+  this.createGroupMembers = function(symptom, concept, value) {
+    var groupMembers = [{
+      concept: symptom.conceptQuestion,
+      value: concept.conceptId
+    }, {
+      concept: symptom.conceptAnswer,
+      value: value
+    }];
+    return groupMembers;
   }
 
   // Yeah, I know!
@@ -60,14 +93,16 @@ module.factory("observationsFactory", function(conceptMappingFactory) {
     return type === "non-coded";
   };
 
-  this.findAnswer = function(answers, value) {
-    var response;
+  this.findAnswer = function(answers, value, hasMultipleOptions) {
+    hasMultipleOptions = (typeof hasMultipleOptions !== 'undefined' ? hasMultipleOptions : false );
+
+    var response = [];
     angular.forEach(answers, function(answer) {
-      if (answer.id === value) {
-        response = answer.conceptId;
+      if (answer.id === value || value[answer.id]) {
+        response.push(answer.conceptId);
       }
     });
-    return response;
+    return (hasMultipleOptions) ? response : response[0];
   };
 
   return {
