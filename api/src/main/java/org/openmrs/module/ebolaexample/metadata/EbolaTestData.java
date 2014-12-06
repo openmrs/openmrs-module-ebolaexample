66a9cf9bd71bf98ad02e3e134d25637635f00c9e
Darius Jazayeri <jazayeri@alum.mit.edu>
2014-12-06 11:03:26 +0100

    Reorganized test data, and completed test case for WardResource

--- api/src/main/java/org/openmrs/module/ebolaexample/metadata/EbolaTestData.java
+++ api/src/main/java/org/openmrs/module/ebolaexample/metadata/EbolaTestData.java
@@ -1,33 +1,125 @@
 package org.openmrs.module.ebolaexample.metadata;
 
-import org.openmrs.Concept;
-import org.openmrs.ConceptName;
-import org.openmrs.api.ConceptService;
+import org.openmrs.GlobalProperty;
+import org.openmrs.Location;
+import org.openmrs.LocationTag;
+import org.openmrs.Person;
+import org.openmrs.PersonName;
+import org.openmrs.Provider;
+import org.openmrs.api.AdministrationService;
+import org.openmrs.api.LocationService;
+import org.openmrs.api.PersonService;
+import org.openmrs.api.ProviderService;
+import org.openmrs.api.UserService;
+import org.openmrs.module.emrapi.EmrApiConstants;
+import org.openmrs.module.metadatadeploy.MetadataUtils;
 import org.openmrs.module.metadatadeploy.bundle.AbstractMetadataBundle;
+import org.openmrs.module.metadatadeploy.bundle.Requires;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-import java.util.Locale;
+import java.util.HashSet;
+import java.util.Set;
 
+/**
+ * This class is assumed to run only against a new disposable database (i.e. an in-memory test db), and it does
+ * things not normally allowed in a Metadata Deploy package, like creating users (without checking if they already
+ * exist).
+ */
 @Component
+@Requires(EbolaMetadata.class)
 public class EbolaTestData extends AbstractMetadataBundle {
 
+    public static final String DOCTOR_PROVIDER_IDENTIFIER = "12345";
+
     @Autowired
-    ConceptService conceptService;
+    private LocationService locationService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private ProviderService providerService;
+
+    @Autowired
+    private PersonService personService;
+
+    @Autowired @Qualifier("adminService")
+    AdministrationService administrationService;
+
+    public static class _Location {
+        public static final String EBOLA_TREATMENT_CENTER = "c035d67e-5830-11e4-af12-660e112eb3f5";
+
+        public static final String SUSPECT_WARD = "534fed82-5831-11e4-af12-660e112eb3f5";
+        public static final String CONFIRMED_WARD = "ed5c93e0-5830-11e4-af12-660e112eb3f5";
+        public static final String RECOVERY_WARD = "0fd9102e-5831-11e4-af12-660e112eb3f5";
+
+        public static final String SUSPECT_BED_1 = "08afd00c-7d2b-11e4-a1ff-1439e1bf193f";
+        public static final String CONFIRMED_BED_1 = "0e47c89e-7d2b-11e4-a1ff-1439e1bf193f";
+        public static final String CONFIRMED_BED_2 = "12ad63c6-7d2b-11e4-a1ff-1439e1bf193f";
+        public static final String RECOVERY_BED_1 = "17a7bffc-7d2b-11e4-a1ff-1439e1bf193f";
+    }
 
     @Override
     public void install() throws Exception {
-        // We cannot do install(concept(...)) because of: java.lang.UnsupportedOperationException: Concepts can only be fetched for now
-        conceptService.saveConcept(concept("Ebola Program", "162637AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", "N/A", "Program"));
+        administrationService.saveGlobalProperty(new GlobalProperty(EmrApiConstants.GP_ADMISSION_ENCOUNTER_TYPE, EbolaMetadata._EncounterType.EBOLA_TREATMENT_ADMISSION));
+
+
+        String supportsAdmission = locationService.getLocationTagByName(EmrApiConstants.LOCATION_TAG_SUPPORTS_ADMISSION).getUuid();
+        String supportsTransfer = locationService.getLocationTagByName(EmrApiConstants.LOCATION_TAG_SUPPORTS_TRANSFER).getUuid();
+        String inpatientBed = EbolaMetadata._LocationTag.INPATIENT_BED;
+
+        install(location("Ebola Treatment Unit", "Entire facility", _Location.EBOLA_TREATMENT_CENTER, null,
+                EbolaMetadata._LocationTag.VISIT_LOCATION));
+
+        install(location("Suspect Ward", null, _Location.SUSPECT_WARD, _Location.EBOLA_TREATMENT_CENTER,
+                supportsAdmission, supportsTransfer, EbolaMetadata._LocationTag.EBOLA_SUSPECT_WARD));
+        install(location("Bed #1", null, _Location.SUSPECT_BED_1, _Location.SUSPECT_WARD,
+                supportsTransfer, inpatientBed));
+
+        install(location("Confirmed Ward", null, _Location.CONFIRMED_WARD, _Location.EBOLA_TREATMENT_CENTER,
+                supportsAdmission, supportsTransfer, EbolaMetadata._LocationTag.EBOLA_CONFIRMED_WARD));
+        install(location("Bed #1", null, _Location.CONFIRMED_BED_1, _Location.CONFIRMED_WARD,
+                supportsTransfer, inpatientBed));
+        install(location("Bed #2", null, _Location.CONFIRMED_BED_2, _Location.CONFIRMED_WARD,
+                supportsTransfer, inpatientBed));
+
+        install(location("Recovery Ward", null, _Location.RECOVERY_WARD, _Location.EBOLA_TREATMENT_CENTER,
+                supportsTransfer, EbolaMetadata._LocationTag.EBOLA_RECOVERY_WARD));
+        install(location("Bed #1", null, _Location.RECOVERY_BED_1, _Location.RECOVERY_WARD,
+                supportsTransfer, inpatientBed));
+
+        createProvider(new PersonName("MoH", "", "Doctor"), DOCTOR_PROVIDER_IDENTIFIER);
     }
 
-    private Concept concept(String name, String uuid, String datatype, String conceptClass) {
-        Concept concept = new Concept();
-        concept.addName(new ConceptName(name, Locale.ENGLISH));
-        concept.setUuid(uuid);
-        concept.setDatatype(conceptService.getConceptDatatypeByName(datatype));
-        concept.setConceptClass(conceptService.getConceptClassByName(conceptClass));
-        return concept;
+    private void createProvider(PersonName name, String providerIdentifier) {
+        Person person = new Person();
+        person.addName(name);
+        person.setGender("F");
+        personService.savePerson(person);
+
+        Provider provider = new Provider();
+        provider.setPerson(person);
+        provider.setIdentifier(providerIdentifier);
+        providerService.saveProvider(provider);
     }
 
+    public static Location location(String name, String description, String uuid, String parentLocationUuid, String... tagUuids) {
+        Location obj = new Location();
+        obj.setName(name);
+        obj.setDescription(description);
+        obj.setUuid(uuid);
+        if (parentLocationUuid != null) {
+            obj.setParentLocation(MetadataUtils.existing(Location.class, parentLocationUuid));
+        }
+        Set<LocationTag> tags = new HashSet<LocationTag>();
+        if (tagUuids != null) {
+            for (String tagUuid : tagUuids) {
+                tags.add(MetadataUtils.existing(LocationTag.class, tagUuid));
+            }
+        }
+        obj.setTags(tags);
+        return obj;
+    }
 }
--- omod/src/test/java/org/openmrs/module/ebolaexample/rest/WardResourceTest.java
+++ omod/src/test/java/org/openmrs/module/ebolaexample/rest/WardResourceTest.java
@@ -2,14 +2,40 @@ package org.openmrs.module.ebolaexample.rest;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.openmrs.EncounterRole;
+import org.openmrs.Location;
+import org.openmrs.Patient;
+import org.openmrs.Provider;
+import org.openmrs.Visit;
+import org.openmrs.api.PatientService;
+import org.openmrs.api.ProviderService;
+import org.openmrs.module.ebolaexample.api.BedAssignmentService;
 import org.openmrs.module.ebolaexample.metadata.EbolaMetadata;
+import org.openmrs.module.ebolaexample.metadata.EbolaTestBaseMetadata;
 import org.openmrs.module.ebolaexample.metadata.EbolaTestData;
+import org.openmrs.module.emrapi.adt.AdtAction;
+import org.openmrs.module.emrapi.adt.AdtService;
+import org.openmrs.module.metadatadeploy.MetadataUtils;
 import org.openmrs.module.webservices.rest.SimpleObject;
 import org.openmrs.module.webservices.rest.web.RequestContext;
+import org.openmrs.module.webservices.rest.web.representation.Representation;
 import org.openmrs.web.test.BaseModuleWebContextSensitiveTest;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+
+import static org.apache.commons.beanutils.PropertyUtils.getProperty;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+
 public class WardResourceTest extends BaseModuleWebContextSensitiveTest {
+
+    @Autowired
+    private EbolaTestBaseMetadata ebolaTestBaseMetadata;
 
     @Autowired
     private EbolaMetadata ebolaMetadata;
@@ -17,20 +43,78 @@ public class WardResourceTest extends BaseModuleWebContextSensitiveTest {
     @Autowired
     private EbolaTestData ebolaTestData;
 
+    @Autowired
+    AdtService adtService;
+
+    @Autowired
+    BedAssignmentService bedAssignmentService;
+
+    @Autowired
+    PatientService patientService;
+
+    @Autowired
+    ProviderService providerService;
+
     private WardResource resource;
 
     @Before
     public void setUp() throws Exception {
-        ebolaTestData.install();
+        ebolaTestBaseMetadata.install();
         ebolaMetadata.install();
+        ebolaTestData.install();
         resource = new WardResource();
     }
 
     @Test
     public void testGetAll() throws Exception {
         RequestContext requestContext = new RequestContext();
+        requestContext.setRepresentation(Representation.REF);
         SimpleObject all = resource.getAll(requestContext);
-        System.out.println(all);
+
+        List<SimpleObject> results = (List<SimpleObject>) all.get("results");
+
+        // just test one to make sure it's right
+        SimpleObject ward = findWithUuid(results, EbolaTestData._Location.SUSPECT_WARD);
+        assertNotNull(ward);
+        assertThat((String) ward.get("type"), is("suspect"));
+        assertThat((String) ward.get("display"), is("Suspect Ward"));
+    }
+
+    @Test
+    public void testGetOne() throws Exception {
+        Location ward = MetadataUtils.existing(Location.class, EbolaTestData._Location.SUSPECT_WARD);
+        Location bed = MetadataUtils.existing(Location.class, EbolaTestData._Location.SUSPECT_BED_1);
+
+        Patient patient = patientService.getPatient(7);
+        admit(patient, ward);
+        bedAssignmentService.assign(patient, bed);
+
+        RequestContext requestContext = new RequestContext();
+        SimpleObject result = (SimpleObject) resource.retrieve(EbolaTestData._Location.SUSPECT_WARD, requestContext);
+
+        assertThat((String) result.get("uuid"), is(EbolaTestData._Location.SUSPECT_WARD));
+        assertThat((String) result.get("type"), is("suspect"));
+        List<SimpleObject> bedAssignments = (List<SimpleObject>) result.get("bedAssignments");
+        assertThat(bedAssignments.size(), is(1));
+        assertThat((String) getProperty(bedAssignments.get(0), "patient.uuid"), is(patient.getUuid()));
+        assertThat((String) getProperty(bedAssignments.get(0), "bed.uuid"), is(bed.getUuid()));
+    }
+
+    private void admit(Patient patient, Location ward) {
+        Visit visit = adtService.ensureActiveVisit(patient, ward);
+        HashMap<EncounterRole, Set<Provider>> providers = new HashMap<EncounterRole, Set<Provider>>();
+        providers.put(MetadataUtils.existing(EncounterRole.class, EbolaMetadata._EncounterRole.CLINICIAN),
+                Collections.singleton(providerService.getProviderByIdentifier(EbolaTestData.DOCTOR_PROVIDER_IDENTIFIER)));
+        adtService.createAdtEncounterFor(new AdtAction(visit, ward, providers, AdtAction.Type.ADMISSION));
+    }
+
+    private SimpleObject findWithUuid(List<SimpleObject> results, String uuid) {
+        for (SimpleObject candidate : results) {
+            if (uuid.equals(candidate.get("uuid"))) {
+                return candidate;
+            }
+        }
+        return null;
     }
 
 }
