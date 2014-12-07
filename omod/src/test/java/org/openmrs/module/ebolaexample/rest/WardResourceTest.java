d37aaee0aee550a65d8bf20cd22bce293ff26642
Darius Jazayeri <jazayeri@alum.mit.edu>
2014-12-07 15:38:35 -0500

    Refactor resource test, and introduce a base class

--- omod/src/test/java/org/openmrs/module/ebolaexample/rest/WardResourceTest.java
+++ omod/src/test/java/org/openmrs/module/ebolaexample/rest/WardResourceTest.java
@@ -1,50 +1,25 @@
 package org.openmrs.module.ebolaexample.rest;
 
-import org.junit.Before;
 import org.junit.Test;
-import org.openmrs.EncounterRole;
 import org.openmrs.Location;
 import org.openmrs.Patient;
-import org.openmrs.Provider;
-import org.openmrs.Visit;
 import org.openmrs.api.PatientService;
-import org.openmrs.api.ProviderService;
 import org.openmrs.module.ebolaexample.api.BedAssignmentService;
-import org.openmrs.module.ebolaexample.metadata.EbolaMetadata;
-import org.openmrs.module.ebolaexample.metadata.EbolaTestBaseMetadata;
 import org.openmrs.module.ebolaexample.metadata.EbolaTestData;
-import org.openmrs.module.emrapi.adt.AdtAction;
-import org.openmrs.module.emrapi.adt.AdtService;
 import org.openmrs.module.metadatadeploy.MetadataUtils;
 import org.openmrs.module.webservices.rest.SimpleObject;
-import org.openmrs.module.webservices.rest.web.RequestContext;
-import org.openmrs.module.webservices.rest.web.representation.Representation;
-import org.openmrs.web.test.BaseModuleWebContextSensitiveTest;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Set;
+import java.util.Map;
 
 import static org.apache.commons.beanutils.PropertyUtils.getProperty;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
-public class WardResourceTest extends BaseModuleWebContextSensitiveTest {
-
-    @Autowired
-    private EbolaTestBaseMetadata ebolaTestBaseMetadata;
-
-    @Autowired
-    private EbolaMetadata ebolaMetadata;
-
-    @Autowired
-    private EbolaTestData ebolaTestData;
-
-    @Autowired
-    AdtService adtService;
+public class WardResourceTest extends BaseEbolaResourceTest {
 
     @Autowired
     BedAssignmentService bedAssignmentService;
@@ -52,29 +27,13 @@ public class WardResourceTest extends BaseModuleWebContextSensitiveTest {
     @Autowired
     PatientService patientService;
 
-    @Autowired
-    ProviderService providerService;
-
-    private WardResource resource;
-
-    @Before
-    public void setUp() throws Exception {
-        ebolaTestBaseMetadata.install();
-        ebolaMetadata.install();
-        ebolaTestData.install();
-        resource = new WardResource();
-    }
-
     @Test
     public void testGetAll() throws Exception {
-        RequestContext requestContext = new RequestContext();
-        requestContext.setRepresentation(Representation.REF);
-        SimpleObject all = resource.getAll(requestContext);
-
-        List<SimpleObject> results = (List<SimpleObject>) all.get("results");
+        SimpleObject all = toSimpleObject(handle(request(GET, "ward")));
+        List<Map> results = (List<Map>) all.get("results");
 
         // just test one to make sure it's right
-        SimpleObject ward = findWithUuid(results, EbolaTestData._Location.SUSPECT_WARD);
+        Map ward = findWithUuid(results, EbolaTestData._Location.SUSPECT_WARD);
         assertNotNull(ward);
         assertThat((String) ward.get("type"), is("suspect"));
         assertThat((String) ward.get("display"), is("Suspect Ward"));
@@ -89,8 +48,7 @@ public class WardResourceTest extends BaseModuleWebContextSensitiveTest {
         admit(patient, ward);
         bedAssignmentService.assign(patient, bed);
 
-        RequestContext requestContext = new RequestContext();
-        SimpleObject result = (SimpleObject) resource.retrieve(EbolaTestData._Location.SUSPECT_WARD, requestContext);
+        SimpleObject result = toSimpleObject(handle(request(GET, "ward/" + EbolaTestData._Location.SUSPECT_WARD)));
 
         assertThat((String) result.get("uuid"), is(EbolaTestData._Location.SUSPECT_WARD));
         assertThat((String) result.get("type"), is("suspect"));
@@ -98,23 +56,6 @@ public class WardResourceTest extends BaseModuleWebContextSensitiveTest {
         assertThat(bedAssignments.size(), is(1));
         assertThat((String) getProperty(bedAssignments.get(0), "patient.uuid"), is(patient.getUuid()));
         assertThat((String) getProperty(bedAssignments.get(0), "bed.uuid"), is(bed.getUuid()));
-    }
-
-    private void admit(Patient patient, Location ward) {
-        Visit visit = adtService.ensureActiveVisit(patient, ward);
-        HashMap<EncounterRole, Set<Provider>> providers = new HashMap<EncounterRole, Set<Provider>>();
-        providers.put(MetadataUtils.existing(EncounterRole.class, EbolaMetadata._EncounterRole.CLINICIAN),
-                Collections.singleton(providerService.getProviderByIdentifier(EbolaTestData.DOCTOR_PROVIDER_IDENTIFIER)));
-        adtService.createAdtEncounterFor(new AdtAction(visit, ward, providers, AdtAction.Type.ADMISSION));
-    }
-
-    private SimpleObject findWithUuid(List<SimpleObject> results, String uuid) {
-        for (SimpleObject candidate : results) {
-            if (uuid.equals(candidate.get("uuid"))) {
-                return candidate;
-            }
-        }
-        return null;
     }
 
 }
