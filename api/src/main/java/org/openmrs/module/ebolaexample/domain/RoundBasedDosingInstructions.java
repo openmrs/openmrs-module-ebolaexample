8bb62b25afa7a95584be3a194464e312f03b9c52
andreweskeclarke <andreweskeclarke@gmail.com>
2014-12-10 10:48:13 +0300

    Adding RoundBasedDosingInstructions to accomodate round based prescription frequencies in ETCs

---
+++ api/src/main/java/org/openmrs/module/ebolaexample/domain/RoundBasedDosingInstructions.java
@@ -0,0 +1,133 @@
+package org.openmrs.module.ebolaexample.domain;
+
+import org.openmrs.*;
+import org.openmrs.api.APIException;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Locale;
+
+public class RoundBasedDosingInstructions implements DosingInstructions {
+
+    private Double dose;
+
+    private Concept doseUnits;
+
+    private Concept route;
+
+    private Integer duration;
+
+    private Concept durationUnits;
+
+    private Boolean asNeeded;
+
+    private String asNeededCondition;
+
+    private String roundInstructions;
+
+    @Override
+    public String getDosingInstructionsAsString(Locale locale) {
+
+        StringBuilder dosingInstructions = new StringBuilder();
+        dosingInstructions.append(this.dose);
+        dosingInstructions.append(" ");
+        dosingInstructions.append(this.doseUnits.getName(locale).getName());
+        dosingInstructions.append(" ");
+        dosingInstructions.append(this.route.getName(locale).getName());
+        if (duration != null) {
+            dosingInstructions.append(" ");
+            dosingInstructions.append(this.duration);
+            dosingInstructions.append(" ");
+            dosingInstructions.append(this.durationUnits.getName(locale).getName());
+        }
+        if (this.asNeeded) {
+            dosingInstructions.append(" ");
+            dosingInstructions.append("PRN");
+            if (this.asNeededCondition != null) {
+                dosingInstructions.append(" ");
+                dosingInstructions.append(this.asNeededCondition);
+            }
+        }
+        dosingInstructions.append(" ");
+        dosingInstructions.append(this.roundInstructions);
+        return dosingInstructions.toString();
+    }
+
+    @Override
+    public void setDosingInstructions(DrugOrder order) {
+
+    }
+
+    @Override
+    public DosingInstructions getDosingInstructions(DrugOrder order) {
+        if (!order.getDosingType().equals(this.getClass())) {
+            throw new APIException("Dosing type of drug order is mismatched. Expected:" + this.getClass().getName()
+                    + " but received:" + order.getDosingType());
+        }
+        RoundBasedDosingInstructions roundBasedDosingInstructions = new RoundBasedDosingInstructions();
+        roundBasedDosingInstructions.setDose(order.getDose());
+        roundBasedDosingInstructions.setDoseUnits(order.getDoseUnits());
+        roundBasedDosingInstructions.setDuration(order.getDuration());
+        roundBasedDosingInstructions.setDurationUnits(order.getDurationUnits());
+        roundBasedDosingInstructions.setAsNeeded(order.getAsNeeded());
+        roundBasedDosingInstructions.setAsNeededCondition(order.getAsNeededCondition());
+        roundBasedDosingInstructions.setRoute(order.getRoute());
+        roundBasedDosingInstructions.setRoundInstructions(order.getDosingInstructions());
+        return roundBasedDosingInstructions;
+    }
+
+    @Override
+    public void validate(DrugOrder order, Errors errors) {
+        ValidationUtils.rejectIfEmpty(errors, "dosingInstructions", "DrugOrder.error.dosingInstructionsIsNullForDosingTypeSimple");
+        ValidationUtils.rejectIfEmpty(errors, "route", "DrugOrder.error.routeIsNullForDosingTypeSimple");
+        ValidationUtils.rejectIfEmpty(errors, "dose", "DrugOrder.error.doseIsNullForDosingTypeSimple");
+        ValidationUtils.rejectIfEmpty(errors, "doseUnits", "DrugOrder.error.doseUnitsIsNullForDosingTypeSimple");
+
+        String[] rounds = order.getDosingInstructions().split(",");
+        for (String round : rounds) {
+            boolean validRoundName = Arrays.asList(new String[]{"Morning", "Afternoon", "Evening", "Night"}).contains(round);
+            if(!validRoundName) {
+                errors.rejectValue("dosingInstructions", "DrugOrder.error.dosingInstructionsInAnInvalidFormat");
+            }
+        }
+    }
+
+    @Override
+    public Date getAutoExpireDate(DrugOrder order) {
+        return null;
+    }
+
+    public void setDose(Double dose) {
+        this.dose = dose;
+    }
+
+    public void setDoseUnits(Concept doseUnits) {
+        this.doseUnits = doseUnits;
+    }
+
+    public void setRoute(Concept route) {
+        this.route = route;
+    }
+
+    public void setDuration(Integer duration) {
+        this.duration = duration;
+    }
+
+    public void setDurationUnits(Concept durationUnits) {
+        this.durationUnits = durationUnits;
+    }
+
+    public void setAsNeeded(Boolean asNeeded) {
+        this.asNeeded = asNeeded;
+    }
+
+    public void setAsNeededCondition(String asNeededCondition) {
+        this.asNeededCondition = asNeededCondition;
+    }
+
+    public void setRoundInstructions(String administrationInstructions) {
+        this.roundInstructions = administrationInstructions;
+    }
+}
