22445a0bda170a14a7b15f74e35429003bfa3412
andreweskeclarke <andreweskeclarke@gmail.com>
2014-12-11 14:28:11 +0300

    Moving angular app constants to their own module

--- omod/src/main/webapp/resources/scripts/tabletapp/app.js
+++ omod/src/main/webapp/resources/scripts/tabletapp/app.js
@@ -1,6 +1,6 @@
 var OPENMRS_CONTEXT_PATH = location.pathname.substring(1, location.pathname.indexOf("/", 1));
 
-angular.module("tabletapp", ["ui.router", "ngResource", "ngDialog", "uicommons.widget.select-drug"])
+angular.module("tabletapp", ["ui.router", "ngResource", "ngDialog", "uicommons.widget.select-drug", "constants"])
 
     .config(function ($stateProvider, $urlRouterProvider) {
 
@@ -130,55 +130,6 @@ angular.module("tabletapp", ["ui.router", "ngResource", "ngDialog", "uicommons.w
             });
         };
     }])
-
-    .factory("Constants", function () {
-        return {
-            encounterType: {
-                ebolaInpatientFollowup: "83413734-587d-11e4-af12-660e112eb3f5"
-            },
-            careSetting: {
-                inpatient: "c365e560-c3ec-11e3-9c1a-0800200c9a66"
-            },
-            dosingType: {
-                freeText: "org.openmrs.FreeTextDosingInstructions",
-                simple: "org.openmrs.SimpleDosingInstructions",
-                roundBased: "org.openmrs.module.ebolaexample.domain.RoundBasedDosingInstructions"
-            },
-            orderType: {
-                drugorder: "drugorder"
-            },
-            rounds: [
-                { name: "Morning"},
-                { name: "Afternoon"},
-                { name: "Evening"},
-                { name: "Night"}
-            ],
-            doseUnits: [
-                {
-                    display: "mg",
-                    uuid: "161553AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
-                }
-            ],
-            routes: [
-                {
-                    display: 'Rectal suppository',
-                    uuid: '162458AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
-                },
-                {
-                    display: 'IV',
-                    uuid: '160242AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
-                },
-                {
-                    display: 'IM',
-                    uuid: '160243AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
-                },
-                {
-                    display: 'Oral',
-                    uuid: '160240AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
-                }
-            ]
-        }
-    })
 
     .factory("CurrentSession", ["$http", "Constants", "EncounterResource",
         function ($http, Constants, EncounterResource) {
