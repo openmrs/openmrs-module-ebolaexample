9dfbbcee236a16760e99dcc2319430c52f714211
Glauber Ramos <glauberamos@gmail.com>
2014-12-15 12:40:57 +0300

    changing button sizes

--- omod/src/main/webapp/resources/scripts/tabletapp/directives.js
+++ omod/src/main/webapp/resources/scripts/tabletapp/directives.js
@@ -2,7 +2,7 @@ angular.module("directives", [])
     .directive('backButton', function () {
         return {
             link: function (scope, element, attrs) {
-                element.html('<button class="left">Back</button>');
+                element.html('<button class="left small">Back</button>');
                 element.bind('click', goBack);
 
                 function goBack() {
