f7fd1cf97ce6c77eef4ca0852182b0b81015291c
andreweskeclarke <andreweskeclarke@gmail.com>
2014-12-24 08:29:39 -0600

    Using dialog to stop drug orders

--- omod/src/main/webapp/resources/scripts/tabletapp/patients.js
+++ omod/src/main/webapp/resources/scripts/tabletapp/patients.js
@@ -122,13 +122,28 @@ angular.module("patients", ["ui.router", "resources", "ngDialog", "constants", "
             };
 
             $scope.stopOrder = StopOrderService.stopOrder;
+
+            $scope.showStoppingError = function() {
+                $scope.problemStopping = true;
+            }
+
+            $scope.openStopOrderDialog = function (order) {
+                $scope.order = order;
+                ngDialog.open({
+                    template: "stopOrderDialog",
+                    controller: "PatientController",
+                    className: "ngdialog-theme-plain",
+                    closeByDocument: false,
+                    scope: $scope
+                });
+            };
         }])
 
     .factory('StopOrderService', ['CurrentSession', 'OrderResource', 'Constants',
         function (CurrentSession, OrderResource, Constants) {
 
             return {
-                stopOrder: function (order) {
+                stopOrder: function (order, success, error) {
                     CurrentSession.getEncounter(order.patient.uuid).then(function (encounter) {
                         CurrentSession.getInfo().then(function (response) {
                             orderJson = {
@@ -139,12 +154,19 @@ angular.module("patients", ["ui.router", "resources", "ngDialog", "constants", "
                                 "encounter": encounter.uuid,
                                 "careSetting": Constants.careSetting.inpatient,
                                 "orderer": response.data.providers[0]["uuid"],
-                                "previousOrder": order.uuid,
-                                "drug": order.drug.uuid
+                                "previousOrder": order.uuid
                             };
 
+                            if(order.drug) {
+                                orderJson["drug"] = order.drug.uuid;
+                            } else {
+                                orderJson["concept"] = order.concept.uuid;
+                            }
+
                             new OrderResource(orderJson).$save().then(function (order) {
-                                console.log(order);
+                                success();
+                            }, function(response) {
+                                error();
                             });
                         });
                     });
