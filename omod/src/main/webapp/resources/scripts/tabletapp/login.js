1b68febce677e9db55c0530fb37cec64363f3618
andreweskeclarke <andreweskeclarke@gmail.com>
2015-01-07 16:37:21 -0600

    ER-57 Login from the UI using an angular service

--- omod/src/main/webapp/resources/scripts/tabletapp/login.js
+++ omod/src/main/webapp/resources/scripts/tabletapp/login.js
@@ -1,6 +1,17 @@
 angular.module("login", [])
-    .controller("LoginController", ["$scope", "UserResource", "Constants",
-        function ($scope, UserResource, Constants) {
-            $scope.login = function () {};
+    .controller("LoginController", ["$scope", "UserResource", "Constants", "LoginService",
+        function ($scope, UserResource, Constants, LoginService) {
+            $scope.loginForm = {};
+            $scope.login = function (loginModel) {
+                LoginService.login(loginModel.username, loginModel.provider);
+            };
             $scope.teams = UserResource.query({role: Constants.roles.wardRoundingTeam});
-        }]);
+        }])
+
+    .service("LoginService", ["$http", function($http) {
+        return {
+            login: function(username, provider) {
+                $http.post("/" + OPENMRS_CONTEXT_PATH + "/ws/rest/v1/ebola/login", {'username': username, 'provider': provider});
+            }
+        }
+    }]);
