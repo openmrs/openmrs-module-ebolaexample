e465ddbf6f9bf31172eebb23cdde0857160cff2f
andreweskeclarke <andreweskeclarke@gmail.com>
2015-01-08 17:00:07 -0600

    ER-57 Send user to wards page after login

--- omod/src/main/webapp/resources/scripts/tabletapp/login.js
+++ omod/src/main/webapp/resources/scripts/tabletapp/login.js
@@ -8,13 +8,14 @@ angular.module("login", [])
             $scope.teams = UserResource.query({role: Constants.roles.wardRoundingTeam});
         }])
 
-    .service("LoginService", ["$http", "CurrentSession",
-        function ($http, CurrentSession) {
+    .service("LoginService", ["$http", "CurrentSession", "$state",
+        function ($http, CurrentSession, $state) {
             return {
                 login: function (username, provider) {
                     $http.post("/" + OPENMRS_CONTEXT_PATH + "/ws/rest/v1/ebola/login", {'username': username, 'provider': String(provider)})
                         .success(function (response) {
                             CurrentSession.setInfo(response);
+                            $state.go("wards");
                         });
                 }
             }
