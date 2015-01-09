4b232a6cad9eb1a211b1cc4bdbe178423776405d
andreweskeclarke <andreweskeclarke@gmail.com>
2015-01-09 14:12:09 -0600

    ER-57 Remove ngPermission

--- omod/src/main/webapp/resources/scripts/tabletapp/tablet-permission.js
+++
@@ -1,6 +0,0 @@
-angular.module('tabletPermissions', ['permission', 'session'])
-    .run(['Permission', 'CurrentSession', function (Permission, CurrentSession) {
-        Permission.defineRole('loggedIn', function (stateParams) {
-            return CurrentSession.getInfo();
-        });
-    }]);
