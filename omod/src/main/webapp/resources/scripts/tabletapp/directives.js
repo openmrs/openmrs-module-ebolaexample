d7dd508933abc4292f507337a59be3f9b38e112e
andreweskeclarke <andreweskeclarke@gmail.com>
2015-01-12 15:17:07 -0600

    ER-90 Add cancel button in the prescription forms instead of a back button

--- omod/src/main/webapp/resources/scripts/tabletapp/directives.js
+++ omod/src/main/webapp/resources/scripts/tabletapp/directives.js
@@ -1,21 +1,36 @@
 angular.module("directives", [])
-    .directive('backButton', ['$state', 'CurrentSession', function ($state, CurrentSession) {
+    .factory('BackService', ['CurrentSession', function (CurrentSession) {
+        return {
+            buildHandler: function ($state) {
+                return function()
+                {
+                    var params = $state.params;
+                    params['uuid'] = CurrentSession.getRecentWard();
+                    $state.go($state.current.data.back.target, params, {reload: true});
+                }
+            }
+        };
+    }])
+    .directive('backButton', ['$state', 'BackService', function ($state, BackService) {
         return {
             link: function (scope, element, attrs) {
                 element.html('<div class="tablet-navigation">' +
-                                '<button class="left small">' +
-                                    '<i class="fa fa-chevron-left"></i>' +
-                                    $state.current.data.back.description +
-                                '</button>' +
-                            '</div>');
-                element.bind('click', goBack);
-
-                function goBack() {
-                    var params = $state.params;
-                    params['uuid'] = CurrentSession.getRecentWard();
-                    $state.go($state.current.data.back.target, params);
-                    scope.$apply();
-                }
+                    '<button class="left small">' +
+                    '<i class="fa fa-chevron-left"></i>' +
+                    $state.current.data.back.description +
+                    '</button>' +
+                    '</div>');
+                element.bind('click', BackService.buildHandler($state));
+            }
+        }
+    }])
+    .directive('cancelButton', ['$state', 'BackService', function ($state, BackService) {
+        return {
+            link: function (scope, element, attrs) {
+                element.html('<button class="left small secondary">' +
+                    'Cancel' +
+                    '</button>');
+                element.bind('click', BackService.buildHandler($state));
             }
         }
     }]);
