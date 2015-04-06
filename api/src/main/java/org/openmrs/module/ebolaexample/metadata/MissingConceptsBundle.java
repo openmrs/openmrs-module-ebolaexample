b3b42d03c6b55ff41057f1a6f604557822fff7a3
Darius Jazayeri <jazayeri@alum.mit.edu>
2015-04-06 17:37:10 -0400

    Only create missing CIEL concept if it is actually missing. (Apparently metadatadeploy doesn't handle the existing concept case well, and I don't have time to investigate.)

--- api/src/main/java/org/openmrs/module/ebolaexample/metadata/MissingConceptsBundle.java
+++ api/src/main/java/org/openmrs/module/ebolaexample/metadata/MissingConceptsBundle.java
@@ -33,29 +33,32 @@ public class MissingConceptsBundle extends AbstractMetadataBundle {
         Concept negative = MetadataUtils.existing(Concept.class, "664AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
         Concept positive = MetadataUtils.existing(Concept.class, "703AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
 
-        ConceptName longName = new ConceptNameBuilder("126373BBBBBBBBBBBBBBBBBBBBBBBBBBBBBB")
-                .name("Ebola virus RNA by polymerase chain reaction")
-                .type(ConceptNameType.FULLY_SPECIFIED)
-                .locale(Locale.ENGLISH)
-                .localePreferred(true).build();
+        String ebolaPcrTestUuid = "162599AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+        if (MetadataUtils.possible(Concept.class, ebolaPcrTestUuid) == null) {
+            ConceptName longName = new ConceptNameBuilder("126373BBBBBBBBBBBBBBBBBBBBBBBBBBBBBB")
+                    .name("Ebola virus RNA by polymerase chain reaction")
+                    .type(ConceptNameType.FULLY_SPECIFIED)
+                    .locale(Locale.ENGLISH)
+                    .localePreferred(true).build();
 
-        ConceptName synonym = new ConceptNameBuilder("126372BBBBBBBBBBBBBBBBBBBBBBBBBBBBBB")
-                .name("Ebola virus PCR")
-                .locale(Locale.ENGLISH)
-                .localePreferred(false).build();
+            ConceptName synonym = new ConceptNameBuilder("126372BBBBBBBBBBBBBBBBBBBBBBBBBBBBBB")
+                    .name("Ebola virus PCR")
+                    .locale(Locale.ENGLISH)
+                    .localePreferred(false).build();
 
-        Concept ebolaLabTest = new ConceptBuilder("162599AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
-                .datatype(coded)
-                .conceptClass(test)
-                .name(longName)
-                .name(synonym)
-                .description("5d7643fb-a4d5-485f-82c4-bf41a16113ab", "Ebola virus PCR in any sample", Locale.ENGLISH)
-                .answers(indeterminate, negative, positive)
-                .mapping(new ConceptMapBuilder("beb8d5a8-3a61-42e8-803e-cab0ec60be33")
-                        .type(sameAs).ensureTerm(ciel, "162599").build())
-                .build();
-        ebolaLabTest.setConceptId(162599);
-        install(ebolaLabTest);
+            Concept ebolaLabTest = new ConceptBuilder(ebolaPcrTestUuid)
+                    .datatype(coded)
+                    .conceptClass(test)
+                    .name(longName)
+                    .name(synonym)
+                    .description("5d7643fb-a4d5-485f-82c4-bf41a16113ab", "Ebola virus PCR in any sample", Locale.ENGLISH)
+                    .answers(indeterminate, negative, positive)
+                    .mapping(new ConceptMapBuilder("beb8d5a8-3a61-42e8-803e-cab0ec60be33")
+                            .type(sameAs).ensureTerm(ciel, "162599").build())
+                    .build();
+            ebolaLabTest.setConceptId(162599);
+            install(ebolaLabTest);
+        }
     }
 
 }
